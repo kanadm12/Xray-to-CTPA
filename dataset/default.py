8bbf503fa4ce1a494ca82aa78530c1edb11b8700
kanadm12 <kanadmukherjee12@gmail.com>
2025-11-21 22:43:49 +0530

    Revert to original 256x256x32, n_hiddens=128, downsample=[2,2,2] - working settings

--- dataset/default.py
+++ dataset/default.py
@@ -7,7 +7,7 @@ import argparse
 
 PREPROCESSING_TRANSORMS = tio.Compose([
     tio.RescaleIntensity(out_min_max=(-1, 1)),
-    tio.CropOrPad(target_shape=(320, 320, 40))  # Balanced: more detail than 256x256x32, fits in memory
+    tio.CropOrPad(target_shape=(256, 256, 32))
 ])
 
 TRAIN_TRANSFORMS = tio.Compose([
