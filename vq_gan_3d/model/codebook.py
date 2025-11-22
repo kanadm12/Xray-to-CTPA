ee451be460628c32971e43a151732474609b2ecd
kanadm12 <kanadmukherjee12@gmail.com>
2025-11-22 22:21:18 +0530

    Fix codebook collapse: reduce n_codes to 512, embedding_dim to 64, increase commitment loss to 1.0, lr to 1e-4

--- vq_gan_3d/model/codebook.py
+++ vq_gan_3d/model/codebook.py
@@ -73,7 +73,7 @@ class Codebook(nn.Module):
             encoding_indices_reshaped, self.embeddings)  # [b, t, h, w, c]
         embeddings = shift_dim(embeddings, -1, 1)  # [b, c, t, h, w]
 
-        commitment_loss = 0.25 * F.mse_loss(z, embeddings.detach())
+        commitment_loss = 1.0 * F.mse_loss(z, embeddings.detach())
 
         # EMA codebook update
         if self.training:
