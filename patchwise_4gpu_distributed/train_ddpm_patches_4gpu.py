6c06e7baec7cfae625c6a445ff072b6cca0f322f
kanadm12 <kanadmukherjee12@gmail.com>
2025-11-28 17:09:11 +0530

    Fix DDP unused parameters error - enable find_unused_parameters=True

--- patchwise_4gpu_distributed/train_ddpm_patches_4gpu.py
+++ patchwise_4gpu_distributed/train_ddpm_patches_4gpu.py
@@ -175,9 +175,9 @@ def main_wrapper():
             LearningRateMonitor(logging_interval='step')
         ]
         
-        # DDP Strategy
+        # DDP Strategy (enable find_unused_parameters for discriminator/classifier)
         ddp_strategy = DDPStrategy(
-            find_unused_parameters=cfg.model.get('find_unused_parameters', False),
+            find_unused_parameters=True,  # Required: discriminator/classifier not always used
             gradient_as_bucket_view=True,
             static_graph=False,
         )
